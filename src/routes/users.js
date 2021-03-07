622f509d7c67026d0063346907aaa0484640a07c
Daviel Jimenez <Daviel050399@gmail.com>
2021-03-07 04:19:09 -0400

    adding path to replace user data

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,10 +2,12 @@ const router = require("express-promise-router")()
 const {
     index,
     newUser,
-    getUser
+    getUser,
+    replaceUser
 } = require("../controllers/user")
 router.get("/", index)
 router.post("/", newUser)
 router.get("/:userid", getUser)
+router.put("/:userid", replaceUser)
 
 module.exports = router;
