595eb065b9e3300b5230871dcf4939f1488ce538
Daviel Jimenez <Daviel050399@gmail.com>
2021-03-07 04:14:20 -0400

    Adding path to get a user

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,11 @@
 const router = require("express-promise-router")()
 const {
     index,
-    newUser
+    newUser,
+    getUser
 } = require("../controllers/user")
 router.get("/", index)
 router.post("/", newUser)
+router.get("/:userid", getUser)
 
 module.exports = router;
