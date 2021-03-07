c62d867b7b28d3403d8a8577751e583d5ef0dfbe
Daviel Jimenez <Daviel050399@gmail.com>
2021-03-07 05:11:01 -0400

    Finished project

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
-const user = require("../models/user")
 const User = require("../models/user")
+const Car = require("../models/cars")
+
 
 module.exports = {
 
@@ -41,5 +42,23 @@ module.exports = {
         const { userid } = req.params
         await user.findByIdAndDelete(userid)
         res.status(200).json({ succes: true })
+    },
+    getUsersCars: async(req, res, next) => {
+        const { userid } = req.params
+        const user = await User.findById(userid).populate("cars")
+        res.status(200).json(user)
+    },
+    newUserCar: async(req, res, next) => {
+        const { userid } = req.params
+        const newCar = new Car(req.body)
+        const user = await User.findById(userid)
+
+        newCar.seller = user
+        await newCar.save()
+
+        user.cars.push(newCar)
+        await user.save()
+
+        res.status(201).json(newCar)
     }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,13 +4,17 @@ const {
     newUser,
     getUser,
     replaceUser,
-    deleteUser
+    deleteUser,
+    getUsersCars,
+    newUserCar
 } = require("../controllers/user")
 router.get("/", index)
 router.post("/", newUser)
 router.get("/:userid", getUser)
 router.put("/:userid", replaceUser)
 router.delete("/:userid", deleteUser)
+router.get("/:userid/cars", getUsersCars)
+router.post("/:userid/cars", newUserCar)
 
 
 module.exports = router;
